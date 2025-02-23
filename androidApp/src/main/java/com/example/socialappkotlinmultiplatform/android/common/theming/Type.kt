ffe0b463f228bd46dd44394540e4370f196d1f40
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-23 16:10:51 +0300

    created the val OpenSans and Lexend where i loaded the fonts

--- androidApp/src/main/java/com/example/socialappkotlinmultiplatform/android/common/theming/Type.kt
+++ androidApp/src/main/java/com/example/socialappkotlinmultiplatform/android/common/theming/Type.kt
@@ -1,2 +1,18 @@
 package com.example.socialappkotlinmultiplatform.android.common.theming
 
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.socialappkotlinmultiplatform.android.R
+
+val Lexend = FontFamily(
+    Font(R.font.lexend_medium, FontWeight.Medium),
+    Font(R.font.lexend_semi_bold,FontWeight.SemiBold),
+    Font(R.font.lexend_bold,FontWeight.Bold)
+)
+
+val OpenSans = FontFamily(
+    Font(R.font.lexend_light,FontWeight.Light),
+    Font(R.font.lexend_legular,FontWeight.Normal)
+)
+
