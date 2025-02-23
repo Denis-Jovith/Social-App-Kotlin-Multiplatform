e255e7cb3a34e78ebfdb2ca17e6f6588edc6983c
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-23 19:43:41 +0300

    learned how to use the unfocussedContainerColor which is for controlling the background

--- androidApp/src/main/java/com/example/socialappkotlinmultiplatform/android/common/components/CustomTextField.kt
+++ androidApp/src/main/java/com/example/socialappkotlinmultiplatform/android/common/components/CustomTextField.kt
@@ -1,14 +1,20 @@
 package com.example.socialappkotlinmultiplatform.android.common.components
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import com.example.socialappkotlinmultiplatform.android.common.theming.Gray
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomTextField(
     modifier: Modifier = Modifier,
@@ -24,5 +30,16 @@ fun CustomTextField(
         onValueChange = onValueChange,
         modifier = modifier.fillMaxWidth(),
         textStyle = MaterialTheme.typography.bodyMedium,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = keyboardType
+        ),
+        singleLine = isSingleLine,
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = if (isSystemInDarkTheme()){
+                MaterialTheme.colorScheme.surface
+            }else{
+                Gray
+            }
+        )
     )
 }
