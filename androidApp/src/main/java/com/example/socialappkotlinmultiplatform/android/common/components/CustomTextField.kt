6431d3273e69083100c7cbef48c7215f8a378a1c
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-23 20:22:30 +0300

    added the toggling of the password using PasswordEyeIcon

--- androidApp/src/main/java/com/example/socialappkotlinmultiplatform/android/common/components/CustomTextField.kt
+++ androidApp/src/main/java/com/example/socialappkotlinmultiplatform/android/common/components/CustomTextField.kt
@@ -2,7 +2,6 @@ package com.example.socialappkotlinmultiplatform.android.common.components
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,7 +10,10 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
+import com.example.socialappkotlinmultiplatform.android.R
 import com.example.socialappkotlinmultiplatform.android.common.theming.Gray
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -39,7 +41,21 @@ fun CustomTextField(
                 MaterialTheme.colorScheme.surface
             }else{
                 Gray
-            }
+            },
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent
         )
     )
+}
+
+@Composable
+fun PasswordEyeIcon(
+    isPasswordVisible: Boolean,
+    onPasswordVisibilityToggle: () ->Unit
+){
+    val image = if (isPasswordVisible){
+        painterResource(id = R.drawable.hide_eye_icon_filled)
+    }else{
+        painterResource(id = R.drawable.show_eye_icon_filled)
+    }
 }
