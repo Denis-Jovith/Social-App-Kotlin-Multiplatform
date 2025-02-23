23219d32642795851aa26e81ae730453c26325b7
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-23 17:20:14 +0300

    Passed onchange of the fields

--- androidApp/src/main/java/com/example/socialappkotlinmultiplatform/android/auth/signup/SignUpScreen.kt
+++ androidApp/src/main/java/com/example/socialappkotlinmultiplatform/android/auth/signup/SignUpScreen.kt
@@ -1,15 +1,20 @@
 package com.example.socialappkotlinmultiplatform.android.auth.signup
 
 import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Modifier
 
 @Composable
 fun SignUpScreen(
-    modifier: Modifier= Modifier
+    modifier: Modifier= Modifier,
+    uiState: SignUpUiState,
+    onUsernameChange:(String) -> Unit,
+    onEmailChange:(String) -> Unit,
+    onPasswordChange:(String) -> Unit,
 ) {
+
+
+
 
 }
 
-data class SignUpUiState(
- var username:String = ""
-)
