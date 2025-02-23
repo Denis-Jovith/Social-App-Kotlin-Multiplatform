aa423b7969dc5ce48e3cedd26fd6083c75397af6
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-23 16:32:01 +0300

    addedd the small large and medium

--- androidApp/src/main/java/com/example/socialappkotlinmultiplatform/android/common/theming/Shape.kt
+++ androidApp/src/main/java/com/example/socialappkotlinmultiplatform/android/common/theming/Shape.kt
@@ -1,2 +1,14 @@
 package com.example.socialappkotlinmultiplatform.android.common.theming
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import com.example.socialappkotlinmultiplatform.android.common.theming.LargeSpacing
+import com.example.socialappkotlinmultiplatform.android.common.theming.MediumSpacing
+import com.example.socialappkotlinmultiplatform.android.common.theming.SmallSpacing
+
+
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(SmallSpacing),
+    medium = RoundedCornerShape(MediumSpacing),
+    large = RoundedCornerShape(LargeSpacing)
+)
