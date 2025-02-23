abacd0cb9d0762193bb0a3d820340397b874caa0
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-23 16:58:26 +0300

    in SignUpScreen.kt created the SignUpScreen composable

--- androidApp/src/main/java/com/example/socialappkotlinmultiplatform/android/auth/signup/SignUpScreen.kt
+++ androidApp/src/main/java/com/example/socialappkotlinmultiplatform/android/auth/signup/SignUpScreen.kt
@@ -1,2 +1,11 @@
 package com.example.socialappkotlinmultiplatform.android.auth.signup
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SignUpScreen(
+    modifier: Modifier= Modifier
+) {
+
+}
